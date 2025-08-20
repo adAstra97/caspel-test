32839253a67800adaa43933355f74c00cc27318d
Nika Verman <nikaverman@gmail.com>
2025-08-20 20:09:34 +0300

    feat: add control panel structure

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -9,7 +9,7 @@ export const Container = ({ children, className = '' }: Props) => {
   return (
     <div
       className={cn(
-        'px-4 sm:px-6 xl:px-10 max-w-[1100px] mx-auto min-w-[320px] w-screen xl:w-[70vw]',
+        'px-4 py-3 sm:px-6 xl:px-10 max-w-[1100px] mx-auto min-w-[320px] w-screen xl:w-[70vw]',
         className
       )}
     >
---
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,17 @@
+import { IoMdAdd } from 'react-icons/io';
+
+export const ControlPanel = () => {
+  return (
+    <div className="flex flex-col gap-4 sm:flex-row justify-between mt-3 mb-5 w-full">
+      <input
+        type="search"
+        placeholder="Search.."
+        className="border-b-2 border-text focus:border-button outline-0 flex items-center gap-2 text-white px-4 py-2 min-w-[250px]"
+      />
+      <button className="flex items-center gap-1.5 text-button cursor-pointer border-2 border-button rounded-2xl px-4 py-2 min-w-[100px] justify-center hover:scale-105 will-change-transform transition-transform duration-200">
+        <IoMdAdd size={20} />
+        Add user
+      </button>
+    </div>
+  );
+};
