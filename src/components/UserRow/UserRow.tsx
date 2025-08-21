24ddb9684d85519794e782f6760fe6e238bd43ce
Nika Verman <nikaverman@gmail.com>
2025-08-21 15:01:16 +0300

    feat: add removing logics

--- src/components/UserRow/UserRow.tsx
+++ src/components/UserRow/UserRow.tsx
@@ -3,12 +3,14 @@ import { FiEdit, FiTrash2 } from 'react-icons/fi';
 import { Button } from '../Button/Button';
 
 interface Props {
+  id: string;
   name: string;
   date: string;
   value: number;
+  onRemove: (id: string) => void;
 }
 
-export const UserRow = ({ date, name, value }: Props) => {
+export const UserRow = ({ id, date, name, value, onRemove }: Props) => {
   return (
     <tr className="hover:bg-border/5 hover:transition-colors duration-100 whitespace-nowrap">
       <td className="px-3 py-2 text-size-16 color-text max-w-[150px] truncate">
@@ -28,7 +30,7 @@ export const UserRow = ({ date, name, value }: Props) => {
             className="text-button min-w-0 px-2 border-0 hover:scale-110"
           />
           <Button
-            onClick={() => console.log('delete')}
+            onClick={() => onRemove(id)}
             icon={<FiTrash2 size={20} />}
             className="text-remove min-w-0 px-2 border-0 hover:scale-110"
           />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ import { MOCKED_DATA } from '../shared/constants';
 import type { RowData } from '../shared/types';
 
 const Home = () => {
+  const [users, setUsers] = useState<RowData[]>(MOCKED_DATA);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { formData, setFormData, errors, validate } = useFormValidation({
     name: '',
@@ -27,9 +28,13 @@ const Home = () => {
       value: formData.value ?? 0,
     };
 
-    MOCKED_DATA.unshift(rowData);
+    setUsers((prev) => [...prev, rowData]);
     setFormData({ name: '', date: '', value: null });
     setIsModalOpen(false);
+  };
+
+  const handleRemove = (id: string) => {
+    setUsers((prev) => prev.filter((item) => item.id !== id));
   };
 
   return (
@@ -37,35 +42,42 @@ const Home = () => {
       <h1 className="text-size-32 mb-3 text-center font-light">Manage Data</h1>
       <div className="py-3 px-4 border-2 rounded-2xl border-border shadow-sm shadow-border">
         <ControlPanel onClick={() => setIsModalOpen(true)} />
-        <div className="overflow-x-auto">
-          <table className="w-full">
-            <caption className="mb-3 text-size-20 text-left">
-              Users 2025
-            </caption>
-            <thead className="select-none border-b-2 border-border/5 bg-border/10">
-              <tr>
-                <HeaderCell
-                  title="Name"
-                  onClick={() => console.log('sort by name')}
-                />
-                <HeaderCell
-                  title="Date"
-                  onClick={() => console.log('sort by date')}
-                />
-                <HeaderCell
-                  title="Value"
-                  onClick={() => console.log('sort by value')}
-                />
-                <th className="px-3 py-2 text-size-18">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {MOCKED_DATA.map(({ id, name, date, value }) => (
-                <UserRow key={id} name={name} date={date} value={value} />
-              ))}
-            </tbody>
-          </table>
-        </div>
+        {!!users.length && (
+          <div className="overflow-x-auto">
+            <table className="w-full">
+              <caption className="mb-3 text-size-20 text-left">
+                Users 2025
+              </caption>
+              <thead className="select-none border-b-2 border-border/5 bg-border/10">
+                <tr>
+                  <HeaderCell
+                    title="Name"
+                    onClick={() => console.log('sort by name')}
+                  />
+                  <HeaderCell
+                    title="Date"
+                    onClick={() => console.log('sort by date')}
+                  />
+                  <HeaderCell
+                    title="Value"
+                    onClick={() => console.log('sort by value')}
+                  />
+                  <th className="px-3 py-2 text-size-18">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((item) => (
+                  <UserRow key={item.id} {...item} onRemove={handleRemove} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {!users.length && (
+          <div className="text-size-24 text-center min-h-[138px] flex items-center justify-center">
+            No data found 😢
+          </div>
+        )}
       </div>
       {isModalOpen && (
         <Modal
